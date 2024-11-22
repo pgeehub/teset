870cc92283f2170175bd8bad7289fcd78f59d5ab
Pieter <Pieter@proton.me>
2024-11-22 12:37:04 +0100

    Changed file back to original text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React bla bla bla
+          Learn React
         </a>
       </header>
     </div>
